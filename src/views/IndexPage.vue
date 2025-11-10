b3a1167c87db483357f35afad527b88995646a67
Joelle Hili <joelle.hili@outlook.com>
2025-11-10 10:43:27 +0100

    feat: Added Add Employee Button

--- src/views/IndexPage.vue
+++ src/views/IndexPage.vue
@@ -5,5 +5,8 @@ import { EmployeeTable } from "@/modules/EmployeeTable";
 <template>
   <section class="page employee-index">
     <EmployeeTable />
+    <section class="employee-index__button-group">
+      <button class="elevated employee-index__button">Add Employee</button>
+    </section>
   </section>
 </template>
